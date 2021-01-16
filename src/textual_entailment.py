701961a5de34cae6eee4dcb4172e2d46da4ce7d8
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2021-01-16 12:11:46 -0500

    text-entailment

--- src/textual_entailment.py
+++ src/textual_entailment.py
@@ -1,6 +1,16 @@
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.pair_classification
+
 class TextualEntailment:
     def __init__(self):
-        pass
+        self.predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/mnli_roberta-2020.06.09.tar.gz", "textual_entailment")
 
     def run(self, premise: str, hypothesis: str):
-        pass
+        results = self.predictor.predict(hypothesis, premise).get('probs')
+        e, c, n = results[0], results[1], results[2]
+
+        if (c > e and c > n):
+            return False
+        else:
+            return True
+            
